8914da62824dd7127337245afce2df3c38d47fcb
bhawna89 <21bcs057@iiitdmj.ac.in>
2024-07-19 14:18:35 +0530

    structuring

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import Categories from './Categories'
+import SpecialDishes from './SpecialDishes'
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <Categories />
+      <SpecialDishes />
+    
     </div>
   )
 }
